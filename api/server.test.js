72dc00ff917eaff6ff1a3483da654514874ddc30
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-22 13:19:22 -0700

    [GET] /api/cereals tests

--- api/server.test.js
+++ api/server.test.js
@@ -2,6 +2,9 @@ const request = require('supertest');
 const db = require('../data/db-config');
 const server = require('./server');
 
+const luckyCharms = { cereal_name: 'Lucky Charms' };
+const frostedFlakes = { cereal_name: 'Frosted Flakes' };
+
 beforeAll(async () => {
   await db.migrate.rollback();
   await db.migrate.latest();
@@ -16,10 +19,31 @@ afterAll(async () => {
 });
 
 describe('server.js', () => {
-  describe('[GET] /cereals', () => {
+  describe('[GET] /api/cereals', () => {
     it('responds with status 200 OK', async () => {
       const res = await request(server).get('/cereals');
       expect(res.status).toEqual(200);
     });
-  });  
+    it('returns the right amount of cereals', async () => {
+      let res;
+
+      await db('cereals').insert(luckyCharms);
+      res = await request(server).get('/cereals');
+      expect(res.body).toHaveLength(1);
+
+      await db('cereals').insert(frostedFlakes);
+      res = await request(server).get('/cereals');
+      expect(res.body).toHaveLength(2);
+    });
+    it('returns cereals in correct format', async () => {
+      await db('cereals').insert(luckyCharms);
+      await db('cereals').insert(frostedFlakes);
+      const res = await request(server).get('/cereals');
+      expect(res.body[0]).toMatchObject({ cereal_id: 1, ...luckyCharms });
+      expect(res.body[1]).toMatchObject({ cereal_id: 2, ...frostedFlakes });
+    });
+  });
+  // describe('[POST] /cereals', () => {
+    
+  // });
 });
